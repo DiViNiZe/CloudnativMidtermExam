04d9a089ee6ac96aba34f4625983f788c78492c7
ewqdsacxz <Patric_emmel@outlook.com>
2018-10-21 11:58:16 +0700

    [ADD]init courseService

---
+++ CourseService/src/main/java/sit/ecr/courseservice/CourseserviceApplication.java
@@ -0,0 +1,12 @@
+package sit.ecr.courseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseserviceApplication.class, args);
+	}
+}
---
+++ CourseService/src/test/java/sit/ecr/courseservice/CourseserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package sit.ecr.courseservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourseserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
